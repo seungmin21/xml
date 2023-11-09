0bc9a1355db37782061d63d97b0b2952fc908fe6
ohseungmin <ohmin2886@gmail.com>
2023-11-09 16:25:32 +0900

    sendFile 작성법 연습 및 작동 확인

--- route.js
+++ route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 
 router.get('/', (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.end("hello")
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 module.exports = router;
