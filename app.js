508190ad3ec8a4776fd4dc7854585aacc1202877
ohseungmin <ohmin2886@gmail.com>
2023-11-09 15:03:08 +0900

    서버 생성

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const port = 3040;
+
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end("hello")
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
