ac5b9dd8404f19d1e645e3cb7eb7a1f53d496d81
ohseungmin <ohmin2886@gmail.com>
2023-11-09 15:59:01 +0900

    정적 미들웨어로 route파일을 작성하면 GET 요청 방식을 인식하지 못한다.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = 3040;
 const userRouter = require('./route')
 
-app.use('/', userRouter)
+app.use(express.static('route'));
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
