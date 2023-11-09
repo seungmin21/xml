7dacaf405e1b444765d41fb756f0ebcd48fb2853
ohseungmin <ohmin2886@gmail.com>
2023-11-09 17:37:36 +0900

    json 파일 추가

--- route.js
+++ route.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 
+// index.html 파일 구동
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
-})
+});
+
+
+// json 파일
+router.get('/sub', (req,res) => {
+  const filePath = path.join(__dirname, 'test.json');
+  res.sendFile(filePath);
+});
 
 module.exports = router;
